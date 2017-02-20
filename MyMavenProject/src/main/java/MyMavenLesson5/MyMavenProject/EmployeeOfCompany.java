1c226a553b805e462e5135e684592bb2f1ed57b4
Avedon-007 <a_n_d_y-007@mail.ru>
2017-02-20 17:49:08 +0200

    Lesson 5, JUnit task
    Make Employee class

--- MyMavenProject/src/main/java/MyMavenLesson5/MyMavenProject/EmployeeOfCompany.java
+++ MyMavenProject/src/main/java/MyMavenLesson5/MyMavenProject/EmployeeOfCompany.java
@@ -2,7 +2,33 @@ package MyMavenLesson5.MyMavenProject;
 
 public class EmployeeOfCompany 
 {
-	private String firstName = "";
-	private String lastName = "";
-	private 
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private int age;
+	private int experience;
+	private String position;
+	
+	
+	public EmployeeOfCompany(String firstName, String lastName, String gender, int age, int experience, String position)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.age = age;
+		this.experience = experience;
+		this.position = position;
+	}
+		
+	
+	
+	public String setFirstName(String name)
+	{
+		return name;
+	}
+	
+	public double calculateSalary(double bonus, sickDays, workingDays, )
+	{
+		return salary = ;
+	}
 }
