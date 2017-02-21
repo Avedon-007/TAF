672b11790666748c16c5496ebe322b9bbca7e4d7
Avedon-007 <a_n_d_y-007@mail.ru>
2017-02-21 14:06:47 +0200

    Lesson 5, working with JUnit project

--- MyMavenProject/src/main/java/MyMavenLesson5/MyMavenProject/EmployeeOfCompany.java
+++ MyMavenProject/src/main/java/MyMavenLesson5/MyMavenProject/EmployeeOfCompany.java
@@ -8,9 +8,10 @@ public class EmployeeOfCompany
 	private int age;
 	private int experience;
 	private String position;
+	private double salaryRate;
 	
 	
-	public EmployeeOfCompany(String firstName, String lastName, String gender, int age, int experience, String position)
+	public EmployeeOfCompany(String firstName, String lastName, String gender, int age, int experience, String position, double salaryRate)
 	{
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -18,17 +19,86 @@ public class EmployeeOfCompany
 		this.age = age;
 		this.experience = experience;
 		this.position = position;
+		this.salaryRate = salaryRate;
+	}
+			
+		
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	
+	public double calculateOneDayCost(int generalWorkigDaysInMoth)
+	{
+		return salaryRate/generalWorkigDaysInMoth;
+	}	
+	
+	public double calculateExperianceCoeff()
+	{
+		double experianceCoeff = 0.0;
+		if(experience >= 10) experianceCoeff = 0.5;
+		else if(experience >= 0 && experience < 10)	experianceCoeff = experience/10;		 
+		else System.out.println("ERROR!!! experience value is less 0");
+		return experianceCoeff;
 	}
 		
-	
-	
-	public String setFirstName(String name)
+	public double calculeteBonus()
 	{
-		return name;
+		return (salaryRate * 0.5) * calculateExperianceCoeff();
 	}
 	
-	public double calculateSalary(double bonus, sickDays, workingDays, )
+	public int countRealWorkigDays(int generalWorkigDaysInMoth, int sickDays, int vacationDays) 
 	{
-		return salary = ;
+		return generalWorkigDaysInMoth - sickDays - vacationDays;
+	}
+
+	public double calculateSalary(double salaryRate, double bonus, int sickDays,  int generalWorkigDaysInMoth,
+			int realWorkigDaysInMoth,  int vacationDays)
+	{
+		return calculateOneDayCost(generalWorkigDaysInMoth) * countRealWorkigDays(generalWorkigDaysInMoth, sickDays, vacationDays) +  calculeteBonus();
 	}
 }
