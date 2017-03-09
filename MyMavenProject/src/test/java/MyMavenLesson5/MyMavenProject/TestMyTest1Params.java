49b873a06807fddbb622bf55832cca712bbf0256
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-03-09 02:28:17 +0200

    Lesson 5, working with parameters for JUnit

--- MyMavenProject/src/test/java/MyMavenLesson5/MyMavenProject/TestMyTest1Params.java
+++ MyMavenProject/src/test/java/MyMavenLesson5/MyMavenProject/TestMyTest1Params.java
@@ -1,33 +1,31 @@
 package MyMavenLesson5.MyMavenProject;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-
-
 import java.util.List;
-
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 
 
-//@RunWith(Parameterized.class)
+
+@RunWith(Parameterized.class)
 public class TestMyTest1Params 
 {
 	private double salaryRate;
 	private int generalWorkigDaysInMoth;	
-	
 	private static List <double[]> dataSet1 = new ArrayList<double[]>();
 	private static List <double[]> dataSet2 = new ArrayList<double[]>();
 	private static List <double[]> dataSet3 = new ArrayList<double[]>();
+	private static int[] dataSet4 = {-1, 2, -3};
+	private static List <double[]> dataSet5 = new ArrayList<double[]>();
+	private static List <double[]> dataSet6 = new ArrayList<double[]>();
+	private static List <int[]> dataSet7 = new ArrayList<int[]>();
 	private static EmployeeOfCompany testInstance;
 	
 	@BeforeClass
@@ -47,34 +45,51 @@ public class TestMyTest1Params
 		dataSet3.add(new double[] {0.7, 7, 0.0});
 		dataSet3.add(new double[] {0.8, 8, 0.0});
 		
+		dataSet5.add(new double[] {0, 1, 0, 0.0});
+		dataSet5.add(new double[] {0.4, 1, 8, 0.0});
+		
+		dataSet6.add(new double[] {0, 0});
+		dataSet6.add(new double[] {-1, 1});	//должен тест упасть, так как 1, но не падает
+		dataSet6.add(new double[] {-0.1, 1});
+		
+		dataSet7.add(new int[] {22, 22, 0, 0, 0});
+		dataSet7.add(new int[] {21, 22, 1, 0, 0});
+		dataSet7.add(new int[] {21, 22, 0, 1, 0});
+		dataSet7.add(new int[] {21, 22, 0, 0, 1});
+		dataSet7.add(new int[] {20, 22, 1, 1, 0});
+		dataSet7.add(new int[] {20, 22, 1, 0, 1});
+		dataSet7.add(new int[] {20, 22, 0, 1, 1});
+		dataSet7.add(new int[] {19, 22, 1, 1, 1});
+		dataSet7.add(new int[] {0, 22, 10, 10, 2});
+
 	}
 	
 	
-	
 		
-//	public TestMyTest1Params(double salaryRate, int generalWorkigDaysInMoth)
-//	{
-//		this.salaryRate = salaryRate;
-//		this.generalWorkigDaysInMoth = generalWorkigDaysInMoth;
-//	}
+	public TestMyTest1Params(double salaryRate, int generalWorkigDaysInMoth)
+	{
+		this.salaryRate = salaryRate;
+		this.generalWorkigDaysInMoth = generalWorkigDaysInMoth;
+	}
 	
-//	@Parameterized.Parameters
-//	public static Collection forTestExceptionIsThrownInCalculateOneDayCostClass()
-//	{
-//		return Arrays.asList(new Object[][]{ {0, 22}, {-1, 22} });
-//	}
-//		
-//	// Two methods of handling Exceptions: @Rule and Annotations
-//	@Rule
-//	public ExpectedException thrown = ExpectedException.none();
-//	
-////	@Test
-////	public void testExceptionIsThrownInCalculateOneDayCostClass()
-////	{
-////		thrown.expect(IllegalArgumentException.class);
-////		testInstance.calculateOneDayCost(salaryRate, generalWorkigDaysInMoth);		
-////	}	
+	@Parameterized.Parameters
+	public static Collection forTestExceptionIsThrownInCalculateOneDayCostClass()
+	{
+		return Arrays.asList(new Object[][]{ {0, 22}, {-1, 22} });
+	}
+		
+// Two methods of handling Exceptions: @Rule and Annotations
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 	
+	@Test
+	public void testExceptionIsThrownInCalculateOneDayCostClass()
+	{
+		thrown.expect(IllegalArgumentException.class);
+		testInstance.calculateOneDayCost(salaryRate, generalWorkigDaysInMoth);		
+	}	
+	
+	//Using DataSet1//	
 	@Test
 	public void testCalculateOneDayCost() 
 	{
@@ -85,7 +100,7 @@ public class TestMyTest1Params
 		}
 	}
 			
-	
+	//Using DataSet2//	
 	@Test
 	public void testCalculateExperianceCoeffIfExpirianceMore9Years()
 	{		
@@ -97,6 +112,7 @@ public class TestMyTest1Params
 		}
 	}
 
+	//Using DataSet3//	
 	@Test
 	public void testCalculateExperianceCoeffIfExpirianceLess9Years()
 	{		
@@ -106,83 +122,55 @@ public class TestMyTest1Params
 				"Expiriance coefficient for 1 years of expiriance should be 0.1",
 				buferArry[0], testInstance.calculateExperianceCoeff(buferArry[1]), buferArry[2]);
 		}
+	}	
+	
+	//Using DataSet4//		
+	@Test(expected = IllegalArgumentException.class)
+	public void testExceptionIsThrownInCalculateExperianceCoeffClass()
+	{	// проверка не валидна
+		for (int i = 0; i < dataSet4.length; i++)		
+			assertEquals(IllegalArgumentException.class, testInstance.calculateExperianceCoeff(dataSet4[i]));			
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testExceptionIsThrownInCalculateExperianceCoeffClass()
-	{	
-		int[] dataSet2 = {-1, 2, -3};		// проверка не валидна
-		for (int i = 0; i < dataSet2.length; i++)
+	//Using DataSet5//
+	@Test
+	public void testCalculeteBonus()
+	{		
+		for (double[] buferArry : dataSet5)
 		{
-			assertEquals(IllegalArgumentException.class, testInstance.calculateExperianceCoeff(dataSet2[i]));	
+			// For String Format  https://www.dotnetperls.com/format-java
+			String text = String
+					.format("Bonus for Employee if SalaryRate %2$.1f and Expieiance %3$.1f years should be %1$.1f ! -> ",
+							buferArry[0], buferArry[1], buferArry[2]);
+			// Will use: assertEquals(double, double, tolerance) - if tolerance
+			// 0.2 , so e.g. 14.5 -> 14.4 & 14.6 (0.2 means +-0.1)	
+		assertEquals(text, buferArry[0], testInstance.calculeteBonus(buferArry[1], buferArry[2]), buferArry[3]);
 		}
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	@Test
-	public void testCalculeteBonus()
-	{
-		// Will use:   assertEquals(double, double, tolerance)  - if tolerance 0.2 , so e.g. 14.5 -> 14.4 & 14.6  (0.2 means +-0.1)		
-		assertEquals(
-				"Bonus for Employee if SalaryRate 2250 and Expieiance 5 years should be 562.5! -> ",
-				562.5, testInstance.calculeteBonus(2250.0, 5), 0.0);
-	}
-	
+	//Using DataSet6//
 	@Test(expected = IllegalArgumentException.class)
 	public void testExceptionIsThrownInCalculeteBonusClass()
-	{		
-		testInstance.calculeteBonus(0, 5);
-		testInstance.calculeteBonus(-1, 5);
-		testInstance.calculeteBonus(0, -1);
-		testInstance.calculeteBonus(-1, -1);
+	{	
+		for (double[] buferArry : dataSet6)		
+			testInstance.calculeteBonus(buferArry[0], buferArry[1]);  
 	}
 	
-	
+	//Using DataSet7//
 	@Test
 	public void testCountRealWorkigDays()
 	{		
-		assertEquals(
-				"Real working days should be 22 if the Working days in month 22, Sickdays 0, Vacations days 0 and Missed days 0",
-				22, testInstance.countRealWorkigDays(22, 0, 0, 0));
-		assertEquals(
-				"Real working days should be 21 if the Working days in month 22, Sickdays 1, Vacations days 0 and Missed days 0",
-				21, testInstance.countRealWorkigDays(22, 1, 0, 0));
-		assertEquals(
-				"Real working days should be 21 if the Working days in month 22, Sickdays 0, Vacations days 1 and Missed days 0",
-				21, testInstance.countRealWorkigDays(22, 0, 1, 0));
-		assertEquals(
-				"Real working days should be 21 if the Working days in month 22, Sickdays 0, Vacations days 0 and Missed days 1",
-				21, testInstance.countRealWorkigDays(22, 0, 0, 1));
-		assertEquals(
-				"Real working days should be 20 if the Working days in month 22, Sickdays 1, Vacations days 1 and Missed days 0",
-				20, testInstance.countRealWorkigDays(22, 1, 1, 0));
-		assertEquals(
-				"Real working days should be 20 if the Working days in month 22, Sickdays 1, Vacations days 0 and Missed days 1",
-				20, testInstance.countRealWorkigDays(22, 1, 0, 1));
-		assertEquals(
-				"Real working days should be 20 if the Working days in month 22, Sickdays 0, Vacations days 1 and Missed days 1",
-				20, testInstance.countRealWorkigDays(22, 0, 1, 1));
-		assertEquals(
-				"Real working days should be 19 if the Working days in month 22, Sickdays 1, Vacations days 1 and Missed days 1",
-				19, testInstance.countRealWorkigDays(22, 1, 1, 1));
-		assertEquals(
-				"Real working days should be 0 if the Working days in month 22, Sickdays 10, Vacations days 10 and Missed days 2",
-				0, testInstance.countRealWorkigDays(22, 10, 10, 2));
+		for (int[] buferArry : dataSet7)
+		{
+			String text = String
+					.format("Real working days should be %1$d if the Working days in month %2$d, Sickdays %3$d, Vacations days %4$d and Missed days %5$d",
+							buferArry[0], buferArry[1], buferArry[2],
+							buferArry[3], buferArry[4]);
+			assertEquals(text,
+					buferArry[0], testInstance.countRealWorkigDays(buferArry[1], buferArry[2], buferArry[3], buferArry[4]));
+		}
+		
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
